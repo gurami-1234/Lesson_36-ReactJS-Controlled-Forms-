158624ab34815e23d3b11988ebc4aa4cb831c30d
gurami-1234 <gukadavitadze22@gmail.com>
2024-10-24 18:27:18 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  // const [value,setValue] = useState('HEllo')
+  const [fullname,setFullname] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+
+  // const alertInfo = ()=>{
+  //   console.log(value)
+  // }
+  const signUp = ()=>{
+    console.log(fullname);
+    console.log(email);
+    console.log(password);
+    if(fullname===""){
+      alert("Sheiyvanet fullname")
+    }
+    if(email===""){
+      alert("Sheiyvanet email")
+    }
+    if(password===""){
+      alert("Sheiyvanet password")
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="text" value={value}  onChange={(e)=>setValue(e.target.value)} />
+      <button onClick={alertInfo}>Alert Value</button> */}
+
+      <input type="text" value={fullname} onChange={(e)=>setFullname(e.target.value)} />
+      <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+      <button onClick={signUp}>Sign Up</button>
     </div>
   );
 }
